f40ba178f3c3852d2763c8ca43ccbcecd1f34f8a
Batuhan Çelikkaya <defendtome35@gmail.com>
2024-09-10 16:20:06 +0300

    For phone address profile picture update settings

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -20,20 +20,27 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
+            'phone' => ['nullable', 'string', 'max:20'], // Telefon alanı
+            'address' => ['nullable', 'string', 'max:255'], // Adres alanı
             'photo' => ['nullable', 'mimes:jpg,jpeg,png', 'max:1024'],
         ])->validateWithBag('updateProfileInformation');
 
+        // Profil fotoğrafını güncelleme
         if (isset($input['photo'])) {
             $user->updateProfilePhoto($input['photo']);
         }
 
+        // Eğer email değiştiyse ve kullanıcı doğrulanmışsa
         if ($input['email'] !== $user->email &&
             $user instanceof MustVerifyEmail) {
             $this->updateVerifiedUser($user, $input);
         } else {
+            // Kullanıcı bilgilerini zorla güncelleme (forceFill)
             $user->forceFill([
                 'name' => $input['name'],
                 'email' => $input['email'],
+                'phone' => $input['phone'], // Telefonu güncelleme
+                'address' => $input['address'], // Adresi güncelleme
             ])->save();
         }
     }
@@ -45,12 +52,16 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
      */
     protected function updateVerifiedUser(User $user, array $input): void
     {
+        // Doğrulanmış kullanıcının bilgilerini güncelleme
         $user->forceFill([
             'name' => $input['name'],
             'email' => $input['email'],
+            'phone' => $input['phone'], // Telefonu güncelleme
+            'address' => $input['address'], // Adresi güncelleme
             'email_verified_at' => null,
         ])->save();
 
+        // Email doğrulama bildirimi gönderme
         $user->sendEmailVerificationNotification();
     }
 }
--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -58,7 +58,18 @@
             <x-input id="name" type="text" class="mt-1 block w-full" wire:model="state.name" required autocomplete="name" />
             <x-input-error for="name" class="mt-2" />
         </div>
-
+        <!-- Phone -->
+        <div class="col-span-6 sm:col-span-4">
+            <x-label for="phone" value="{{ __('Phone') }}" />
+            <x-input id="phone" type="text" class="mt-1 block w-full" wire:model="state.phone" required autocomplete="phone" />
+            <x-input-error for="phone" class="mt-2" />
+        </div>  
+        <!-- Address -->
+        <div class="col-span-6 sm:col-span-4">
+            <x-label for="address" value="{{ __('Address') }}" />
+            <x-input id="address" type="text" class="mt-1 block w-full" wire:model="state.address" required autocomplete="address" />
+            <x-input-error for="address" class="mt-2" />
+        </div>
         <!-- Email -->
         <div class="col-span-6 sm:col-span-4">
             <x-label for="email" value="{{ __('Email') }}" />
